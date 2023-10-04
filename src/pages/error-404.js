91801319121ae64fdf31d400479883fe66a6cc37
Manish Rawat <manishrawat@Manishs-Mac-mini.local>
2023-10-04 17:55:32 +0530

    Error Page Design

--- src/pages/error-404.js
+++ src/pages/error-404.js
@@ -2,33 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Error404 = () => {
-    return (
-        <div className="nk-app-root">
-            <div className="nk-main ">
-                <div className="nk-wrap nk-wrap-nosidebar">
-                    <div className="nk-content ">
-                        <div className="nk-block nk-block-middle wide-xs mx-auto">
-                            <div className="nk-block-content nk-error-ld text-center">
-                                <h1 className="nk-error-head">404</h1>
-                                <h3 className="nk-error-title">
-                                    Oops! Why you’re here?
-                                </h3>
-                                <p className="nk-error-text">
-                                    We are very sorry for inconvenience. It
-                                    looks like you’re try to access a page that
-                                    either has been deleted or never existed.
-                                </p>
-                                <Link
-                                    to="/"
-                                    className="btn btn-lg btn-primary mt-2"
-                                >
-                                    Back To Home
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div class="section">
+      <h1 class="error">404</h1>
+      <div class="page">Ooops!!! The page you are looking for is not found</div>
+     <Link to="/">  <a class="back-home" href="#!">
+        Back to home
+      </a>
+      </Link>
+    </div>
+  );
 };
